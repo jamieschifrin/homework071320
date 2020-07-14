feaf7481b2087427432c68af14ed7667b92352cf
Jamie Schrifrin <jamieschifrin@hotmail.com>
2020-07-14 09:23:14 -0400

    first

---
+++ app.js
@@ -0,0 +1,37 @@
+// var Cat = {
+//     "legs": 4,
+//     'color': 'brown',
+//     'likes': ['food','play']
+
+// };
+
+// console.log(Cat.color)
+
+// var dogs = {
+//     "eyes" : 2,
+//     "tail" : "yes",
+//     "friend" : ["man","dogs"]
+// };
+
+// console.log(dogs['tail'])
+
+// var birds = {
+//     "name": "psycho",
+//     "wings": 2,
+//     "enemies": ["Water", "cats"]
+//   };
+
+// console.log(birds['enemies'[1]])
+
+
+
+
+var x = 
+function test(x,y){
+    for (var i = 0; i <= 5; i++) {
+        console.log(x);
+
+};
+
+var z = (1,2,3,4,5);
+console.log(z):
